582e3da88745ecd2c70d221f2bc033f27415f220
Adhe Prabowo <adebiber9@gmail.com>
2026-06-09 05:54:18 +0000

    Perbaiki eror layanan shopee like

--- src/app/dashboard/traffic/likes/page.tsx
+++ src/app/dashboard/traffic/likes/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState, useEffect } from "process"
+import React, { useState, useEffect } from "react"
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
